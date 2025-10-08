ac2ab4865e25968745dd46703bc9770b3192d7a8
Akbar Bagus Wicaksana <110794387+Akbarbagusw@users.noreply.github.com>
2025-10-08 22:20:28 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS2.java
+++ jobsheet5/ifCetakKRS2.java
@@ -7,11 +7,17 @@ public class ifCetakKRS2{
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else{
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // } else{
+        //     System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        // }
+
+        String pesanKRS = uktLunas ? 
+            "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" : 
+            "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesanKRS);
     }
 }
