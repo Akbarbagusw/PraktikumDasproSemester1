6b7b93092b98eb96afdf456ed05aee55831c8b6a
Akbar Bagus Wicaksana <110794387+Akbarbagusw@users.noreply.github.com>
2025-11-16 20:10:21 +0700

    Modifikasi Percobaan 4

--- jobsheet9/SearchNilai2.java
+++ jobsheet9/SearchNilai2.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
 public class SearchNilai2 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan banyaknya nilai yang akan diinput: ");
+        int jmlNilai = sc.nextInt();
+        int[] arrNilai = new int[jmlNilai];
+
+        for (int i = 0; i < jmlNilai; i++){
+            System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.println("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++){
-            if (key == arrNilai[i]){
+            if (arrNilai[i] == key){
                 hasil = i;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu du indeks ke-"+hasil);
-        System.out.println();
+        
+        if (hasil != -1){
+            System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+(hasil+1));
+        } else{
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
     }
 }
