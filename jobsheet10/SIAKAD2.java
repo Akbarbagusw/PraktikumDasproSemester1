484de8829856253cab71e655a25defa48c41b33c
Akbar Bagus Wicaksana <110794387+Akbarbagusw@users.noreply.github.com>
2025-11-19 13:14:59 +0700

    Percobaan 4

--- jobsheet10/SIAKAD2.java
+++ jobsheet10/SIAKAD2.java
@@ -4,9 +4,6 @@ public class SIAKAD2 {
         Scanner sc = new Scanner(System.in);
         int[][] nilai = new int[4][3];
 
-        System.out.println("\n===================================");
-        System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
-
         for (int i = 0; i < nilai.length; i++){
             System.out.println("Input nilai mahasiswa ke-"+(i+1));
             double totalPerSiswa = 0;
@@ -16,6 +13,17 @@ public class SIAKAD2 {
                 totalPerSiswa += nilai[i][j];
             }
             System.out.println("Nilai rata-rata: "+totalPerSiswa/3);
+        }
+
+        System.out.println("\n===================================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
+
+        for (int j = 0; j < 3; j++){
+            double totalPerMatkul = 0;
+            for (int i = 0; i < 4; i++){
+                totalPerMatkul += nilai[i][j];
+            }
+            System.out.println("Mata kuliah "+(j+1)+": "+totalPerMatkul/4);
         }
     }
 }
